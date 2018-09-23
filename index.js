59da514b24dc9ceea4351da757d140256000a6f4
Bill Sowa <billsowa@Bills-MacBook-Pro.local>
2018-09-23 13:57:33 -0400

    update port, add Procfile

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -16,6 +17,6 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
 });
